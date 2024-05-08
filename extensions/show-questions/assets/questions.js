0b201a57b2adf69aa49b3d6aef60dd84b835464b
asim1048 <asimm1048@gmail.com>
2024-05-08 12:28:22 +0500

    running-app-basic-ext

---
+++ extensions/show-questions/assets/questions.js
@@ -0,0 +1,6 @@
+// Select the submit button by its type
+let submitButton = document.querySelector('input[type="submit"]');
+
+submitButton.addEventListener("click", function () {
+   alert("This is from choco app");
+});
